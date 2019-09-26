53a3d9112626ae3d48170330f084c03a7069ed6e
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:41:03 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan10-Logika/src/if1/pkg10118005/latihan10/logika/IF110118005Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan10.logika;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan logika true and false
+ */
+public class IF110118005Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+    
+}
